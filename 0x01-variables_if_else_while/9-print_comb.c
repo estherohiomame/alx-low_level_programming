cc2ba640c36f3e32175c20f5b772c8ef8f9d8e93
estherohiomame <estherohiomame@gmail.com>
2023-06-16 16:14:54 -0700

    C variables 1-7

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *main  - prints all possible combinations of the numbers 0 - 9
+ *Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		putchar(j);
+		if (j != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
