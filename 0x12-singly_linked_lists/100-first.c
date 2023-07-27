faab26594d0f4a575eed735094870fcade8aab90
estherohiomame <estherohiomame@gmail.com>
2023-07-27 05:22:15 -0700

    The hare and the tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void pre_main(void) __attribute__((constructor));
+
+/**
+ * pre_main - Function to be executed before main.
+ */
+void pre_main(void)
+{
+    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
