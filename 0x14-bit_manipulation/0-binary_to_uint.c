5266d1772de543b0c44b64488b415b75376956c8
estherohiomame <100796143+estherohiomame@users.noreply.github.com>
2023-08-03 14:35:52 +0100

    no errors on fourth try

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,21 +8,21 @@
  *         - There is one or more chars in the string b that is not 0 or 1
  *         - b is NULL
  */
+
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int result = 0;
+	int i;
+	unsigned int dec_val = 0;
 
 	if (!b)
 		return (0);
 
-	while (*b)
+	for (i = 0; b[i]; i++)
 	{
-		if (*b != '0' && *b != '1')
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
-
-		result = (result * 2) + (*b - '0');
-		b++;
+		dec_val = (dec_val << 1) | (b[i] - '0');
 	}
 
-	return (result);
+	return (dec_val);
 }
